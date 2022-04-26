9edc25992942c7d66e8e0000a02e8f905063c8db
Ardhan12 <arief.ramadhan43@gmail.com>
2022-04-26 14:32:47 +0700

    Initial Commit

---
+++ Qur'anmu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Qur'anmu
+//
+//  Created by Arief Ramadhan on 26/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
